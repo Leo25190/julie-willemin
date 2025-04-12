4b0ccfaf8ac222799af5a1a112ba51eac2ef1718
Léo DELECROIX <leo.delecroix39@gmail.com>
2025-04-12 23:48:03 +0200

    first commit

---
+++ src/components/Paragraph.tsx
@@ -0,0 +1,28 @@
+import { ChevronDownIcon, ChevronUpIcon } from "@assets/icons"; 
+import { useState } from "react";
+
+
+export default function Paragraph({ text, maxLength = 400 }: { text: string; maxLength?: number }) {
+  const [expanded, setExpanded] = useState(false);
+
+  const shouldTruncate = text.length > maxLength;
+  const displayedText = expanded || !shouldTruncate ? text : text.slice(0, maxLength) + "…";
+
+  return (
+    <div className="mt-5 md:mt-8 mb-5 text-slate-700 text-justify">
+      <p className="intro-bio leading-6 md:leading-7 md:text-xl">
+        {displayedText}
+      </p>
+      {shouldTruncate && (
+        <><button
+          onClick={() => setExpanded(!expanded)}
+          className="text-slate-600 font-semibold hover:underline mt-2"
+        >
+            <div className="flex">
+                {expanded ? <>{ChevronUpIcon} Lire moins</> : <>{ChevronDownIcon} Lire Plus</>}
+            </div>
+        </button>
+      </>)}
+    </div>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,90 @@
+export const isTransitionEnabled = true; // for enabling view transitions
+
+export const myName = 'Julie Willemin'; // For name in main intro text
+
+export const navLogoText = 'Julie Willemin'; // navbar left-most logo text
+// bio for main section
+
+export const myBio = `Ma sensibilité m’a guidé vers le secteur culturel. J’aime aller voir des spectacles de danse, des pièces de théâtre, des concerts ou encore des comédies musicales. Les émotions que je ressens lors de ces moments d’émerveillement sont indescriptibles. Pour continuer sur mes passions, j’aime particulièrement les animaux et surtout les félins. Du chat domestique au tigre, tous me fascinent. J’ai d’ailleurs une femelle Maine Coon à la maison, que j’aime indéfiniment. Côté sport, je suis naturellement attirée par la danse. J’ai pratiqué pendant douze ans le Modern Jazz, puis j’ai fait deux ans de Pole Dance et un an de Contemporain. La danse est pour moi un moyen d’expression, qui m’a permis d’enlever un peu de timidité et de rajouter un peu de confiance. J’ai également fait pas mal de natation et désormais je pratique la course à pied. Chaque course est un nouvel objectif à relever ! Enfin, je dirais que ma dernière passion est de voyager. Les pays asiatiques m’attirent pour leur culture et les pays scandinaves pour leur nature. Côté personnalité, la rigueur et l’investissement sont des mots qui me reflètent. Que ce soit dans la vie privée ou professionnelle, j’offre chaleureusement une part de moi-même. Mes amies me comparent souvent à un petit soleil, car mon énergie et mon sourire leur font du bien. Enfin, ma couleur préférée est le bleu, sûrement parce que je suis une adepte des rêves et des contes de fées. Mon âme d’enfant me permet ainsi de faire des vœux, que j’essaie bien évidemment d’exaucer avec détermination.
+En clair, j’aborde chaque nouvelle expérience comme une scène à explorer, un voyage à entreprendre ou un défi à relever. Si vous cherchez une personne prête à s’investir avec enthousiasme et sérieux, alors rencontrons-nous. Je serais ravie d’écrire avec vous le prochain chapitre de mon parcours !`;
+
+// about me text for about section
+
+export const aboutMe =
+`Après un baccalauréat général, je me suis inscrite à l’ECM de Belfort, une école de Commerce et Management. J’y ai suivi une Licence de Droit, Économie, Gestion, avec un parcours en Commerce, Vente, Marketing. Ce qui m’a permis d’effectuer un stage en banque et une alternance en tant que conseillère de vente. De plus, j’ai acquis de nombreuses compétences en stratégie d’entreprise, en gestion financière, en négociation ou encore en communication. Cependant, je n’y ai pas trouvé ma voie, car la passion n’était pas là.
+Pour poursuivre mes études, j’ai donc choisi l’ICART, une école de médiation culturelle, disposant de cinq campus en France. L’ICART propose un MBA en Ingénierie Culturelle et Management, permettant de se professionnaliser dans le spectacle vivant, la musique, l’art/le patrimoine ou encore le cinéma. Mon projet de quatrième année était l’organisation d’un talent show mêlant danse, chant et théâtre. Celui-ci m’a permis de réaliser une vraie première expérience enrichissante sur le terrain, en amont de l’événement et en live : production, programmation, diffusion, communication... J’ai adoré ! Par ailleurs, j’étais récemment en stage en tant qu’assistante de production, à L’échangeur - CDCN, à Château-Thierry. J’accueillais les artistes en résidence, je rédigeais des conventions, des contrats, à l’occasion d’un festival de danse, je participais à la programmation de l’édition suivante et surtout j’apprenais de nombreuses choses sur la coordination d’événements, la logistique et sur le secteur culturel en général.
+J’aspire à m’exercer davantage dans ce secteur qui m’anime, en découvrant d’autres missions, d’autres métiers et d’autres structures culturelles. Je souhaite ouvrir mon esprit et m’ancrer pleinement dans cet environnement où la créativité et l’organisation se rencontrent pour donner vie à des moments magiques.`;
+
+// A sub text for writing/blog section
+export const blogText = `J’ai créé cet espace pour vous partager ce qui m’inspire. SPECTACLE VIVANT, MUSIQUE, ART, CINÉMA, en passant par mes VOYAGES, mes ACTIVITÉS SPORTIVES et mon amour inconditionnel pour les ANIMAUX… Ce blog est un rideau ouvert sur mon univers.
+À travers des articles illustrés de photos et de vidéos, je vous raconte avec authenticité des découvertes et des expériences marquantes, ainsi que des croyances, qui font de moi celle que je suis. Une personne vraie, sensible, passionnée, positive, curieuse, raisonnée…`;
+
+// A sub text for contact section
+
+export const contactText = `Que ce soit pour échanger sur nos passions, discuter de projets artistiques, me poser des questions ou me conseiller, je serais ravie de vous lire ! N’hésitez pas à me contacter.`;
+
+// Links to put in the navigation bar as well as footer at bottom of the page
+
+export const navLinks = [
+  { name: 'Home', href: '/' },
+  { name: 'About', href: '/#about' },
+  { name: 'Blog', href: '/#blog' },
+  { name: 'Contact', href: '/#contact' },
+];
+
+// Selected projects (side box) for main section of page
+
+export const selectedProjects = [
+  { name: 'Bénévole au TEDx, Belfort', link: '#' },
+  { name: 'Talent show Keraki, Lille', link: '#' },
+];
+
+// Skills section for about section of the page
+
+export const skills = [
+  'Négociation',
+  'Marketing',
+  'Communication',
+  'RH',
+  'Management',
+  'Gestion financière',
+  'Gestion de projet',
+  'Droit Culturel',
+  'Médiation',
+  'Production',
+  'Programmation',
+];
+
+// work experience box for about section of the page
+
+export const workExperiences = [
+  {
+    organisationName: 'company 1',
+    position: 'Intern',
+    date: '2022 - 2023',
+    info: ['I worked on this', 'I showed this skills'],
+  },
+  {
+    organisationName: 'company 2',
+    position: 'UI/UX Designer',
+    date: '2024 - present',
+    info: ['I worked on this', 'I showed this skills'],
+  },
+  {
+    organisationName: 'company 3',
+    position: 'React Developer',
+    date: '2022 - 2023',
+    info: ['I worked on this', 'I showed this skills'],
+  },
+];
+
+// contact box for contact section of the page
+
+export const contactOptions = [
+  { name: 'Tel : 0643233431', link: 'tel:+33643233431' },
+  { name: 'LinkedIN', link: 'https://www.linkedin.com/in/julie-willemin/' },
+  { name: 'Instagram', link: 'https://www.instagram.com/julie_willemin/' },
+];
+
+// for enabling/disabling maintained by text in footer, if you want.
+export const showDeveloperText = true;
